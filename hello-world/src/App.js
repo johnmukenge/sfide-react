2d124fafcadf79100208396e63fcc350f0c0e843
verdesgrobert <github@appboxstudios.com>
2023-01-23 20:04:55 +0100

    Aggiunto input e utilizzato suo valore su p

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
   return (
     <div className="App">
       <p>Hello world</p>
+      Testo: <input value={input} onChange={e => setInput(e.target.value)} />
+      <p className="AppParagraph">Sono un paragrafo dinamico, scrivi qualcosa per vederlo qui sotto: <br/><b>{input}</b></p>
     </div>
   );
 }
