a3d2b72c27f68595d2bc8987804d90fafcbe24bd
MukengeNJ <john.mukenge>
2023-01-25 18:59:12 +0100

    aggiunta classe App

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         <Button words={handleButtons} />
         </form>
       </div>
-      <div>
+      <div className="App">
         {words.map((word) => (
           <button key={word}>{word}</button>
         ))}
